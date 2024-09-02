de48fe139fa49f09ecc68a38e32b32a9c0f52b9d
fabrialberio <->
2024-09-02 18:25:19 +0200

    Added database initialization scripts

---
+++ docker/init_db/001_create_tables.sql
@@ -0,0 +1,74 @@
+USE gestioneordini;
+
+DROP TABLE IF EXISTS Ordini;
+DROP TABLE IF EXISTS Prodotti;
+DROP TABLE IF EXISTS UnitaDiMisura;
+DROP TABLE IF EXISTS Fornitori;
+DROP TABLE IF EXISTS TipologiaProdotto;
+DROP TABLE IF EXISTS Utenti;
+DROP TABLE IF EXISTS RuoloPermesso;
+DROP TABLE IF EXISTS Permessi;
+DROP TABLE IF EXISTS Ruoli;
+
+CREATE TABLE Ruoli (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE Permessi (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE RuoloPermesso (
+    id_ruolo INT,
+    id_permesso INT,
+    PRIMARY KEY (id_ruolo, id_permesso)
+);
+ALTER TABLE RuoloPermesso ADD CONSTRAINT ruolopermesso_id_ruolo_foreign FOREIGN KEY(id_ruolo) REFERENCES Ruoli(id);
+ALTER TABLE RuoloPermesso ADD CONSTRAINT ruolopermesso_id_permesso_foreign FOREIGN KEY(id_permesso) REFERENCES Permessi(id);
+
+CREATE TABLE Utenti (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_ruolo INT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255),
+    nome VARCHAR(255),
+    cognome VARCHAR(255)
+);
+ALTER TABLE Utenti ADD CONSTRAINT utente_id_ruolo_foreign FOREIGN KEY(id_ruolo) REFERENCES Ruoli(id);
+
+CREATE TABLE TipologiaProdotto (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE Fornitori (
+    id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE UnitaDiMisura (
+    id INT PRIMARY KEY,
+    simbolo VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE Prodotti (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_tipologia INT,
+    id_fornitore INT,
+    id_unita_di_misura INT,
+    nome VARCHAR(255)
+);
+ALTER TABLE Prodotti ADD CONSTRAINT prodotti_id_fornitore_foreign FOREIGN KEY(id_fornitore) REFERENCES Fornitori(id);
+ALTER TABLE Prodotti ADD CONSTRAINT prodotti_id_tipologia_foreign FOREIGN KEY(id_tipologia) REFERENCES TipologiaProdotto(id);
+ALTER TABLE Prodotti ADD CONSTRAINT prodotti_id_unita_di_misura_foreign FOREIGN KEY(id_unita_di_misura) REFERENCES UnitaDiMisura(id);
+
+CREATE TABLE Ordini (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_prodotto INT,
+    id_utente INT,
+    quantita BIGINT,
+    data_richiesta DATE
+);
+ALTER TABLE Ordini ADD CONSTRAINT ordini_id_prodotto_foreign FOREIGN KEY(id_prodotto) REFERENCES Prodotti(id);
+ALTER TABLE Ordini ADD CONSTRAINT ordini_id_utente_foreign FOREIGN KEY(id_utente) REFERENCES Utenti(id);
---
+++ docker/init_db/010_insert_data.sql
@@ -0,0 +1,22 @@
+USE gestioneordini;
+
+INSERT INTO Ruoli (id, nome) VALUES
+    (0, "Cuoco"),
+    (1, "Magazziniere"),
+    (2, "Ammministratore");
+
+INSERT INTO Permessi (id, nome) VALUES
+    (0, "vedi_prodotti"),
+    (1, "vedi_tutti_ordini"),
+    (2, "modifica_prodotti"),
+    (3, "modifica_proprio_ordine"),
+    (4, "modifica_tutti_ordini"),
+    (5, "modifica_utenti");
+
+INSERT INTO RuoloPermesso (id_ruolo, id_permesso) VALUES
+    (0, 0),
+    (0, 3),
+    (1, 1),
+    (2, 2),
+    (2, 4),
+    (2, 5);
