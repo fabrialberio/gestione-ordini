a94d7793d29dd81fa6d34f700e7f80734c309017
fabrialberio <->
2024-09-03 11:55:09 +0200

    Implement minimal server

--- main.go
+++ main.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 	"gestione-ordini/database"
 	"log"
+	"net/http"
 	"os"
 	"time"
 )
 
 func main() {
+	db := CreateDatabase()
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+
+	log.Println("Server started on port 8080.")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func CreateDatabase() *database.Database {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:3306)/%s",
 		os.Getenv("MYSQL_USER"),
@@ -20,14 +32,14 @@ func main() {
 	db, err := database.NewDatabase(dsn)
 	for n_retries := 0; err != nil; n_retries++ {
 		if n_retries == 5 {
-			log.Fatalf("Error creating database after %v: %v", n_retries, err)
+			log.Fatalf("Error creating database after %v retries: %v", n_retries, err)
 		}
 
 		time.Sleep(5 * time.Second)
 		log.Printf("Error creating database, retrying: %v", err)
 		db, err = database.NewDatabase(dsn)
 	}
-	defer db.Close()
-
 	log.Println("Database created successfully.")
+
+	return db
 }
