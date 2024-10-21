4996d2267d535c389483fa7301bc7aac83fe4b43
fabrialberio <->
2024-10-21 12:07:57 +0200

    feature: Add props file for handler constants

---
+++ pkg/handlers/props.go
@@ -0,0 +1,15 @@
+package handlers
+
+const (
+	keyUserID       = "id"
+	keyUserRoleID   = "password"
+	keyUserUsername = "username"
+	keyUserPassword = "password"
+	keyUserName     = "name"
+	keyUserSurname  = "surname"
+
+	destAdminUsers       = "/admin/users"
+	destAdminProducts    = "/admin/products"
+	destManagerAllOrders = "/manager/allOrders"
+	destManagerProducts  = "/manager/products"
+)
