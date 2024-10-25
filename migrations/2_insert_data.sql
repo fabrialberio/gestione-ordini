a5c3c945fcacaca14beaefe3f2c366556d1745e4
fabrialberio <->
2024-10-25 17:30:22 +0200

    fix: Typo in column name

--- migrations/2_insert_data.sql
+++ migrations/2_insert_data.sql
@@ -51,7 +51,7 @@ INSERT INTO unita_di_misura (simbolo) VALUES
     ("Kg"),
     ("Lt");
 
-INSERT INTO fornitori (mail, nome) VALUES
+INSERT INTO fornitori (email, nome) VALUES
     ('info@botegadalpan.it', 'Botegadalpan'),
     ('zorteam@email.it', 'Maurizio Z.'),
     ('info@ortofrutticola.com', 'Ortofrutticola Srl'),
