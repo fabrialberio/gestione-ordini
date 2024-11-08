c6fb7710d674cb5227a352e7046a90f9a97f0864
fabrialberio <->
2024-11-08 22:57:30 +0100

    feature: Add more data to list and csv exports

--- pkg/exporters/main.go
+++ pkg/exporters/main.go
@@ -10,13 +10,19 @@ import (
 func ExportToList(orders []database.Order) []byte {
 	builder := bytes.Buffer{}
 
-	for _, order := range orders {
-		builder.WriteString(order.Product.Name)
-		builder.WriteString(" - ")
-		builder.WriteString(order.AmountString)
+	for s, supplierOrders := range splitOrdersBySupplier(orders) {
+		builder.WriteString("Ordini per il fornitore \"" + s.Name + "\" (" + s.Email + "):\n")
+
+		for _, o := range supplierOrders {
+			builder.WriteString("  • ")
+			builder.WriteString(o.Product.Name)
+			builder.WriteString(" - ")
+			builder.WriteString(o.AmountString)
+			builder.WriteString("\n")
+		}
+
 		builder.WriteString("\n")
 	}
-	builder.WriteString("\n")
 
 	return builder.Bytes()
 }
@@ -26,16 +32,31 @@ func ExportToCSV(orders []database.Order) []byte {
 	writer := csv.NewWriter(&builder)
 	writer.Comma = ';'
 
-	writer.Write([]string{"Prodotto", "Quantità", "Unità di misura", "Fornitore"})
-	for _, order := range orders {
-		writer.Write([]string{
-			order.Product.Name,
-			strconv.Itoa(order.Amount),
-			order.Product.UnitOfMeasure.Symbol,
-			order.Product.Supplier.Name,
-		})
+	writer.Write([]string{"Prodotto", "Quantità", "Unità di misura", "Fornitore", "Data di consegna richiesta", "Richiesto da"})
+	for _, supplierOrders := range splitOrdersBySupplier(orders) {
+		for _, order := range supplierOrders {
+			writer.Write([]string{
+				order.Product.Name,
+				strconv.Itoa(order.Amount),
+				order.Product.UnitOfMeasure.Symbol,
+				order.Product.Supplier.Name,
+				order.ExpiresAt.Format("02/01/2006"),
+				order.User.Username,
+			})
+		}
 	}
+
 	writer.Flush()
 
 	return builder.Bytes()
+}
+
+func splitOrdersBySupplier(orders []database.Order) map[database.Supplier][]database.Order {
+	ordersBySupplier := map[database.Supplier][]database.Order{}
+
+	for _, o := range orders {
+		ordersBySupplier[o.Product.Supplier] = append(ordersBySupplier[o.Product.Supplier], o)
+	}
+
+	return ordersBySupplier
 }
