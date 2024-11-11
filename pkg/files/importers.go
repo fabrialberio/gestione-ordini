e18e08e02ef15466b6d823dcc624008bfb296ead
fabrialberio <->
2024-11-11 12:51:44 +0100

    papercut: Remove csvComma const

--- pkg/files/importers.go
+++ pkg/files/importers.go
@@ -15,9 +15,7 @@ var ErrUnexpectedHeader = errors.New("unexpected CSV header")
 var productExpectedHeader = []string{"id", "id_tipologia", "id_fornitore", "id_unita_di_misura", "descrizione", "codice"}
 
 func ImportProductsFromCSV(reader io.Reader, keepIds bool) ([]database.Product, error) {
-	csvReader := csv.NewReader(reader)
-	csvReader.Comma = csvComma
-	csvReader.LazyQuotes = true
+	csvReader := newCustomCsvReader(reader)
 
 	header, err := csvReader.Read()
 	if err != nil {
@@ -68,4 +66,66 @@ func ImportProductsFromCSV(reader io.Reader, keepIds bool) ([]database.Product, 
 	}
 
 	return products, nil
+}
+
+func ImportUsersFromCSV(reader io.Reader, keepIds bool) ([]database.User, error) {
+	csvReader := newCustomCsvReader(reader)
+
+	header, err := csvReader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	if !slices.Equal(header, productExpectedHeader) {
+		return nil, fmt.Errorf("%w: expected %v found %v", ErrUnexpectedHeader, productExpectedHeader, header)
+	}
+
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var products []database.Product
+	for _, r := range records {
+		id, err := strconv.Atoi(r[0])
+		if err != nil {
+			return nil, err
+		}
+		productTypeId, err := strconv.Atoi(r[1])
+		if err != nil {
+			return nil, err
+		}
+		supplierId, err := strconv.Atoi(r[2])
+		if err != nil {
+			return nil, err
+		}
+		unitOfMeasureId, err := strconv.Atoi(r[3])
+		if err != nil {
+			return nil, err
+		}
+
+		product := database.Product{
+			ProductTypeID:   productTypeId,
+			SupplierID:      supplierId,
+			UnitOfMeasureID: unitOfMeasureId,
+			Description:     r[4],
+			Code:            r[5],
+		}
+
+		if keepIds {
+			product.ID = id
+		}
+
+		products = append(products, product)
+	}
+
+	return products, nil
+}
+
+func newCustomCsvReader(reader io.Reader) *csv.Reader {
+	csvReader := csv.NewReader(reader)
+	csvReader.Comma = ';'
+	csvReader.LazyQuotes = true
+
+	return csvReader
 }
