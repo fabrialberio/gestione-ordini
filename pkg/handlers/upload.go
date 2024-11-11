a3a0527e09ac5ba6b382e6653217d65abeb0b615
fabrialberio <->
2024-11-11 12:20:02 +0100

    feature: Implement CSV upload for products

--- pkg/handlers/upload.go
+++ pkg/handlers/upload.go
@@ -6,56 +6,14 @@ import (
 	"gestione-ordini/pkg/components"
 	"gestione-ordini/pkg/database"
 	"gestione-ordini/pkg/files"
+	"mime/multipart"
 	"net/http"
 	"strconv"
 )
 
-func PostUploadPreview(w http.ResponseWriter, r *http.Request) {
-	maxRowCount, err := strconv.Atoi(r.URL.Query().Get("maxRowCount"))
-	if err != nil {
-		maxRowCount = 10
-	}
-
-	f, _, err := r.FormFile("csvFile")
-	if err != nil {
-		logError(r, err)
-		return
-	}
-
-	products, err := files.ImportProductsFromCSV(f)
-	if err != nil {
-		logError(r, err)
-		appContext.ExecuteTemplate(w, r, "uploadError", err.Error())
-		return
-	}
-
-	rows := [][]string{}
-	for _, p := range products {
-		rows = append(rows, []string{
-			strconv.Itoa(p.ID),
-			strconv.Itoa(p.ProductTypeID),
-			strconv.Itoa(p.SupplierID),
-			strconv.Itoa(p.UnitOfMeasureID),
-			p.Description,
-			p.Code,
-		})
-	}
-
-	data := components.PreviewTable{
-		TableURL:    DestUploadPreview,
-		MaxRowCount: maxRowCount,
-		Headings: []components.TableHeading{
-			{Name: "ID"},
-			{Name: "ID Tipologia"},
-			{Name: "ID Fornitore"},
-			{Name: "ID Unità di misura"},
-			{Name: "Descrizione"},
-			{Name: "Codice"},
-		},
-		Rows: rows,
-	}
-
-	appContext.ExecuteTemplate(w, r, "previewTable", data)
+type uploadForm struct {
+	KeepIds bool
+	CSVFile multipart.File
 }
 
 func GetUpload(w http.ResponseWriter, r *http.Request) {
@@ -69,14 +27,108 @@ func GetUpload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := struct {
-		Sidebar []components.SidebarDest
+		Sidebar       []components.SidebarDest
+		KeepIdsSelect components.Select
 	}{
 		Sidebar: currentSidebar(4, true),
+		KeepIdsSelect: components.Select{
+			Name:  "keepIds",
+			Label: "Mantieni ID",
+			Options: []components.SelectOption{
+				{Value: 1, Text: "Sì"},
+				{Value: 0, Text: "No"},
+			},
+		},
 	}
 
 	appContext.ExecuteTemplate(w, r, "upload.html", data)
 }
 
-func PostUpload(w http.ResponseWriter, r *http.Request) {
+func PostUploadPreview(w http.ResponseWriter, r *http.Request) {
+	form, err := parseUploadForm(r)
+	if err != nil {
+		appContext.ExecuteTemplate(w, r, "uploadPlaceholder", nil)
+		return
+	}
 
+	products, err := files.ImportProductsFromCSV(form.CSVFile, form.KeepIds)
+	if err != nil {
+		logError(r, err)
+		appContext.ExecuteTemplate(w, r, "uploadError", err.Error())
+		return
+	}
+
+	rows := [][]string{}
+	for _, p := range products {
+		row := []string{
+			strconv.Itoa(p.ProductTypeID),
+			strconv.Itoa(p.SupplierID),
+			strconv.Itoa(p.UnitOfMeasureID),
+			p.Description,
+			p.Code,
+		}
+
+		if form.KeepIds {
+			row = append([]string{strconv.Itoa(p.ID)}, row...)
+		}
+
+		rows = append(rows, row)
+	}
+
+	headings := []components.TableHeading{
+		{Name: "ID Tipologia"},
+		{Name: "ID Fornitore"},
+		{Name: "ID Unità di misura"},
+		{Name: "Descrizione"},
+		{Name: "Codice"},
+	}
+
+	if form.KeepIds {
+		headings = append([]components.TableHeading{{Name: "ID"}}, headings...)
+	}
+
+	data := components.PreviewTable{
+		TableURL:    DestUploadPreview,
+		MaxRowCount: 8,
+		Headings:    headings,
+		Rows:        rows,
+	}
+
+	appContext.ExecuteTemplate(w, r, "previewTable", data)
+}
+
+func PostUpload(w http.ResponseWriter, r *http.Request) {
+	form, err := parseUploadForm(r)
+	if err != nil {
+		ShowItemInvalidFormError(w, r, err)
+		return
+	}
+
+	products, err := files.ImportProductsFromCSV(form.CSVFile, form.KeepIds)
+	if err != nil {
+		ShowItemInvalidFormError(w, r, err)
+		return
+	}
+
+	err = appContext.Database(r).CreateAllProducts(products)
+	if err != nil {
+		ShowDatabaseQueryError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, DestProducts, http.StatusSeeOther)
+}
+
+func parseUploadForm(r *http.Request) (uploadForm, error) {
+	keepIds := r.FormValue("keepIds") == "1"
+
+	f, _, err := r.FormFile("csvFile")
+	if err != nil {
+		return uploadForm{}, err
+	}
+
+	return uploadForm{
+		KeepIds: keepIds,
+		CSVFile: f,
+	}, nil
 }
